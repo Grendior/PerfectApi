e7650b2534facd63007ba9d90cc2c0967f419674
Szymon Fiedorowicz <szymon.fiedorowicz@gmail.com>
2023-11-15 23:51:26 +0100

    Rejestracja i logowanie

--- src/routes/User/controller.ts
+++ src/routes/User/controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 import User from '../../database/models/User';
 import UserResource from './UserResource';
@@ -24,6 +25,11 @@ export const registration = async (
 ) => {
   const { firstName, lastName, email, password } = req.body;
 
+  const user = await User.findOne({ where: { email } });
+  if (user) {
+    return res.status(409).json({ message: 'User already exist' });
+  }
+
   const hashedPassword = await bcrypt.hash(password, 10);
 
   try {
@@ -34,7 +40,36 @@ export const registration = async (
       password: hashedPassword,
     });
 
-    res.status(201).json({ message: 'User registered successfully', user });
+    return res
+      .status(201)
+      .json({ message: 'User registered successfully', user });
+  } catch (error: any) {
+    next(error);
+  }
+};
+
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ where: { email } });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = jwt.sign(
+        { userId: user.id },
+        process.env.SECRET_KEY as string,
+        {
+          expiresIn: '30d',
+        }
+      );
+      return res.status(200).json({ message: 'Login successful', token });
+    }
+
+    return res.status(401).json({ error: 'Invalid credentials' });
   } catch (error: any) {
     next(error);
   }
--- src/routes/User/index.ts
+++ src/routes/User/index.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
-import { listUsers } from './controller';
+import { listUsers, registration, login } from './controller';
 
 const users: Router = Router();
 
 users.get('/list', listUsers);
+
+users.post('/registration', registration);
+
+users.post('/login', login);
 
 export default users;
